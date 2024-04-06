fc2729f38b8bff7180cbecf49579a5db600ef6d2
Rafael Eduardo Martinez Frontado <103870044+Rzyfront@users.noreply.github.com>
2024-04-06 14:56:40 -0500

    New strucutre for folder proyect

--- peasy_client/src/App.vue
+++ peasy_client/src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import NavbarComponent from './components/NavbarComponent.vue';
+import NavbarComponent from './components/AppComponents/NavbarComponent.vue'
 </script>
 
 <template>
-    <div class="App
-    w-[100%]
-    h-[100vh]
-    flex
-    justify-center
-    items-center
-    ">
-      <NavbarComponent/>
-      <RouterView />
-    </div>
+  <div class="App w-[100%] h-[100vh] flex justify-center items-center">
+    <NavbarComponent />
+    <RouterView />
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
