c47e8db7634759382346557da6723bfc3cefe9ba
Rafael Eduardo Martinez Frontado <103870044+Rzyfront@users.noreply.github.com>
2024-04-06 15:29:42 -0500

    Add Text from Languages.JS

---
+++ peasy_client/src/assets/Languages/HomeView.js
@@ -0,0 +1,12 @@
+//USE LIKE TextContent ON COMPONENTS
+
+//SPANISH
+export const SPANISH_HOMEVIEW = {
+  Title:
+    'Con <span class="text-color3 font-bold cursor-pointer hover:text-color3hover">Peasy</span> hotels admistra tus hoteles, glampings, hostales, Apartamentos y todo lo demas <span class="text-color2 font-bold cursor-pointer hover:text-color3hover">Easy Peasy</span>',
+  AditionalText:
+    'Olvídese de los procesos manuales y las herramientas desorganizadas. Nuestro software de gestión hotelera integral le ofrece una solución completa para optimizar y agilizar las operaciones de su negocio.',
+  ButtoText: 'Adquirir Peasy Hotels ya!',
+  SmallText:
+    '¿Necesitas algo mas para decidirte? <span class="text-color3 font-bold cursor-pointer hover:text-color3hover"> Pruebalo en el simulador.</span>'
+}
--- peasy_client/src/views/HomeView.vue
+++ peasy_client/src/views/HomeView.vue
@@ -1,91 +1,48 @@
 <script setup>
+//Untils
 import { RouterLink } from 'vue-router'
-import hotel3 from '@/assets/images/hotels3.jpg';
 
+//Images
+import hotel3 from '@/assets/images/hotels3.jpg'
+
+//Components
+
+//Languages
+import { SPANISH_HOMEVIEW } from '@/assets/Languages/HomeView'
 </script>
 
 <template>
-  <main class="
-  w-[100%]
-  h-[100vh]
-  flex
-  gap-5
-  justify-center
-  items-center
-  ">
-    <div class="
-        w-[55%]
-        h-[100%]
-        flex
-        flex-col
-        justify-around
-        items-center
-      ">
+  <main class="w-[100%] h-[100vh] flex gap-5 justify-center items-center">
+    <div class="w-[55%] h-[100%] flex flex-col justify-around items-center">
+      <div class="w-[100%] h-[70%] flex flex-col justify-center items-start p-20 gap-10">
+        <div class="w-[90%] flex flex-col gap-4">
+          <h2 class="text-4xl font-bold" v-html="SPANISH_HOMEVIEW.Title"></h2>
 
+          <p class="font-normal text-base">
+            {{ SPANISH_HOMEVIEW.AditionalText }}
+          </p>
+        </div>
 
-    <div class="
-    w-[100%]
-    h-[70%]
-    flex
-    flex-col
-    justify-center
-    items-start
-    p-20
-    gap-10
-    ">
-    <div class="w-[90%] flex flex-col gap-4">
-
-      <h2 class="text-4xl font-bold">
-        Con <span class="text-color3 font-bold cursor-pointer hover:text-color3hover">Peasy</span> hotels admistra tus hoteles, glampings, hostales, Apartamentos y todo lo demas <span class="text-color2 font-bold cursor-pointer hover:text-color3hover">Easy Peasy</span>
-      </h2>
-
-      <p class="font-normal text-base">
-        Olvídese de los procesos manuales y las herramientas desorganizadas. Nuestro software de gestión hotelera integral le ofrece una solución completa para optimizar y agilizar las operaciones de su negocio.
-        
-      </p>
-    </div>
-    
-    
-      <div class="flex flex-col gap-5">
-        <RouterLink to="/Login" class="
-        btn-primary
-        flex
-        justify-center
-        items-center
-        w-[300px]
-        h-[50px]
-        ">
-        Adquirir Peasy Hotels ya!
-      </RouterLink>
-        <p>
-         ¿Necesitas algo mas para decidirte? <span class="text-color3 font-bold cursor-pointer hover:text-color3hover">Pruebalo en el simulador.</span>
-        </p>
-    </div>
-    </div>
-    
+        <div class="flex flex-col gap-5">
+          <RouterLink
+            to="/Login"
+            class="btn-primary flex justify-center items-center w-[300px] h-[50px]"
+          >
+            {{ SPANISH_HOMEVIEW.ButtoText }}
+          </RouterLink>
+          <p v-html="SPANISH_HOMEVIEW.SmallText"></p>
+        </div>
       </div>
-  
-    <div class="
-    w-[45%]
-    h-[100%]
-    flex
-    justify-center
-    items-center
-      ">
-        <img :src="hotel3" alt="Hotel Image"
-        class="
-        w-[100%]
-        h-[100%]
-        overflow-hidden
-        "
-        >
-      </div>
+    </div>
+
+    <div class="w-[45%] h-[100%] flex justify-center items-center">
+      <img :src="hotel3" alt="Hotel Image" class="w-[100%] h-[100%] overflow-hidden" />
+    </div>
   </main>
 </template>
 
 <style scoped>
-
-.custom-style-home-card{
+.custom-style-home-card {
   box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 }
 </style>
