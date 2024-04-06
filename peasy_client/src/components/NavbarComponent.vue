c9b07d95484a23f321d7dc31b0d08d02d6edeb7b
Rafael Eduardo Martinez Frontado <103870044+Rzyfront@users.noreply.github.com>
2024-04-06 14:53:16 -0500

    Landing page

---
+++ peasy_client/src/components/NavbarComponent.vue
@@ -0,0 +1,66 @@
+<script setup>
+ import PeasyLogoHorizontal from '@/assets/images/peasyhorizontallogonobg.png';
+ import { RouterLink } from 'vue-router'
+</script>
+ 
+ 
+<template>
+  <nav class="
+  fixed
+  top-0
+  left-0
+  w-[100%]
+  h-[90px]
+  px-7
+  py-2
+  shadowhover
+  flex
+  justify-between
+  items-center
+  gap-10
+  ">
+  <RouterLink to="/" class="
+  w-[210px]
+  h-[50px]
+  ">
+    <img :src="PeasyLogoHorizontal" alt="Peasy Image" class="
+        w-[100%]
+        h-[100%]
+        ">
+  </RouterLink>
+
+  <div class="flex justify-between w-[100%] items-center">
+    
+      <div class="flex gap-10 items-center">
+        <RouterLink to="/" class="font-medium text-color2 border-b-2 border-transparent hover:border-color2 transition duration-150 ease-in-out">
+          Planes
+        </RouterLink>
+        <RouterLink to="/" class="font-medium text-color2 border-b-2 border-transparent hover:border-color2 transition duration-150 ease-in-out">
+          Simular
+        </RouterLink>
+        <RouterLink to="/" class="font-medium text-color2 border-b-2 border-transparent hover:border-color2 transition duration-150 ease-in-out">
+          ¿Tienes dudas?
+        </RouterLink>
+        <RouterLink to="/" class="font-medium text-color2 border-b-2 border-transparent hover:border-color2 transition duration-150 ease-in-out">
+          Acerca de Peasy Hotels
+        </RouterLink>
+     </div>
+   
+
+    <div class="flex gap-10 items-center">
+      <RouterLink to="/" class="font-medium text-neutral-50 border-b-2 border-transparent hover:border-neutral-50 transition duration-150 ease-in-out">
+        Login
+      </RouterLink>
+      <RouterLink to="/" class="btn-bordered-light">
+        Register
+      </RouterLink>
+    </div>
+
+  </div>
+  </nav>
+</template>
+ 
+ 
+<style scoped>
+
+</style>
--- peasy_client/tailwind.config.js
+++ peasy_client/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'color1' : 'White',
+        'color2' : '#1E232C',
+        'color3' : '#35C2C1',
+        'color3hover' : '#0085c0',
+        'color4' : '#6A707C',
+        'color5' : '#8391A1',
+        'color6' : '#E8ECF4',
+      },
+ 
+    },
   },
   plugins: [],
 }
